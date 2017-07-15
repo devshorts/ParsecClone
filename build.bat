87302d1c8aafe31bb9f3e353f29dd2fca63666d4
devshorts <akropp@gmail.com>
2017-07-15 15:12:02 -0700

    Updating build and build script

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
 
 msbuild /p:Configuration=Release /p:Platform="Any CPU" ParsecClone.sln
 
-"testrunner/NUnit.Runners.2.6.4/tools/nunit-console-x86.exe"  /framework:net-4.5./Samples/FsTests/bin/Release/FsTests.dll
+"testrunner/NUnit.Runners.2.6.4/tools/nunit-console-x86.exe"  /framework:net-4.5 ./Samples/FsTests/bin/Release/FsTests.dll
 
